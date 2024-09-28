a71eb879a0b0cb7282a5c46c07b1aad473b9b837
David Nugent <davidn@uniquode.io>
2024-09-28 21:08:19 +1000

    Refactor templates and blocks for enhanced layout and flexibility
    This commit aims to improve layout flexibility and code maintainability.
    
    - Squashed migrations
    - Template refinements
    - refined various HTML templates
    - Moved SCSS into cmspage from main app
    - Introduced new utility blocks and background options for improved block customization
    - registering a new rich text feature for custom font use.

--- cmspage/wagtail_hooks.py
+++ cmspage/wagtail_hooks.py
@@ -1,10 +1,12 @@
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
-from wagtail.forms import forms
-from wagtail.snippets.models import register_snippet
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail import hooks
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
+from wagtail.forms import forms
+from wagtail.rich_text import LinkHandler
+from wagtail.snippets.models import register_snippet
 
 from .views import CarouselImageSnippetViewSet, MenuLinkViewSet, SiteVariablesViewSet
-
 
 register_snippet(CarouselImageSnippetViewSet)
 
@@ -37,3 +39,37 @@ def construct_site_panels(request, site, panels):
             )
         )
     return panels
+
+
+class UsefontClassHandler(LinkHandler):
+    identifier = "usefont"
+
+    @staticmethod
+    def expand_db_attributes(attrs):
+        return f"<span class=\"usefont\">{attrs.get('href')}</span>"
+
+
+def register_custom_class_feature(features):
+    feature_name = "usefont"
+    type_ = "USEFONT"
+    tag = "usefont"
+
+    control = {
+        "type": type_,
+        "label": "𝕱",
+        "description": "Use special font",
+        "style": {"backgroundColor": "lightcyan"},
+    }
+
+    features.register_editor_plugin(
+        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    db_conversion = {
+        "from_database_format": {tag: InlineStyleElementHandler(type_)},
+        "to_database_format": {"style_map": {type_: tag} }
+    }
+    features.register_converter_rule("content_state", feature_name, db_conversion)
+    features.default_features.append(feature_name)
+
+hooks.register("register_rich_text_features", register_custom_class_feature)
